8192fc24115ab187efb91dcd05aceae14ca26074
Abhishek Vaishnav <abhishekvaishnav93246@gmail.com>
2025-04-29 16:48:05 +0530

    change in app.py

--- app.py
+++ app.py
@@ -219,9 +219,10 @@ def download_video(filename):
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(
         debug=True,
         use_reloader=False,
         host="0.0.0.0",
-        port=5000
+        port=port
     )
